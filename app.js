c9188fc6ea314ed7c1481ff6a5f123328ca6ccae
DineshReddyMaddula <dineshreddymaddula@gmail.com>
2017-08-22 11:14:00 -0700

    second

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 var app = require('express')(),
     parse = require('body-parser'),
     mongoose = require('mongoose');
+var auth = require('./config.json');
+mongoose.connect("mongodb://"+auth.user+":"+auth.pass+"@ds163612.mlab.com:63612/application");
+mongoose.connection.on('connected',function () {
+    console.log("Successfully Connected To DataBase");
+});
+mongoose.connection.on('error',function () {
+    console.log("Something went wrong");
+});
 
 app.listen(8080,function () {
     console.log("Server Started");
